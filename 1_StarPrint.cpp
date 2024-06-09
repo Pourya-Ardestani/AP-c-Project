914aa99dd77cfb576703c2f1d8485da270337a6c
Pourya-Ardestani <pooryaardestani@gmail.com>
2024-06-09 21:36:04 +0330

    first file now works correcly

--- 1_StarPrint.cpp
+++ 1_StarPrint.cpp
@@ -28,19 +28,19 @@ int main()
     Star third(2);
     Star forth(1);
  
-    Star listOfInstances[] = {&first, &second, &third, &forth};
-    Star* p ;
-    p = &listOfInstances ;
-    for(int i = 0 ; i < 4; i++)
-    {
-        p->print_star();
-        p+=i*4 ; 
-    }
+    // Star listOfInstances[] = {&first, &second, &third, &forth};
+    // Star* p ;
+    // p = &listOfInstances ;
+    // for(int i = 0 ; i < 4; i++)
+    // {
+    //     p->print_star();
+    //     p+=i*4 ; 
+    // }
 
-    // first.print_star();
-    // second.print_star();
-    // third.print_star();
-    // forth.print_star();
+    first.print_star();
+    second.print_star();
+    third.print_star();
+    forth.print_star();
 
     return 0;
 }
