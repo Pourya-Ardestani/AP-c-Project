dd76d9125d718a40c597ad57b921c4ec0ca5d5b7
Pourya-Ardestani <pooryaardestani@gmail.com>
2024-06-11 13:43:48 +0330

    Student names sort by grades complited

--- 2-StudentsGrade.cpp
+++ 2-StudentsGrade.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 
 void sort(vector<float> & );
-void printVector(vector<float> &);
 
 class Student
 {
@@ -15,6 +14,25 @@ class Student
         Student(string, float );
         void setGrade(float);
         float getGrade();
+        string getName();
+};
+
+
+class ClassLesson
+{
+    private:
+        vector<Student> listofStudents ;
+        vector<float> grades ;
+        int countofStudents ;
+    public:
+        ClassLesson(int);
+        void setStudents();
+        void sortStudents();
+        void printStudentsName();
+        void printStudentGrade();
+        void printGrade(vector<float> &);
+        void printNames(vector<Student> &);
+
 };
 
 int main()
@@ -22,22 +40,11 @@ int main()
     int numberOfStudents ;
     cout<< "please enter quantity of Student in class:";
     cin >> numberOfStudents; 
-    string new_name;
-    float new_grade ;
-    vector<float> grades ;
-    for (int i = 0; i < numberOfStudents; i++)
-    {
-        cout << i+1 <<". student: "<< endl; 
-        cout << "Enter his/her name: ";
-        cin >> new_name ;
-        cout << "Enter grade: ";
-        cin >> new_grade;
-        Student a(new_name, new_grade);
-        cout << endl;
-        grades.push_back(new_grade) ;
-    }
-    sort(grades);
-    printVector(grades);
+    ClassLesson fizic(numberOfStudents);
+    cout << endl ;
+    fizic.printStudentGrade();
+    cout << endl ;
+    fizic.printStudentsName();
 
     return 0;
 }
@@ -73,12 +80,83 @@ void sort(vector<float> & grades)
         }
     }
 }
-void printVector(vector<float> &vec)
+
+void ClassLesson::printGrade(vector<float> &vec)
 {
-    cout << "sorted grades are: ";
-    for (int i=0;  i < vec.size();  i++)
+    for (int i=0; i < vec.size();  i++)
     {
-        cout<<vec.at(i)<<" ";
+        cout << vec.at(i) <<" ";
     }
     cout<<endl;
 }
+
+void ClassLesson::printNames(vector<Student> & vec)
+{
+    for (int i=0; i < vec.size();  i++)
+    {
+        cout << vec.at(i).getName() <<" ";
+    }
+    cout<<endl;
+	
+}
+
+string Student::getName()
+{
+	return name;
+}
+
+
+//ClassLesson methods: 
+
+ClassLesson::ClassLesson(int c)
+{
+    countofStudents = c ;
+    setStudents();
+}
+void ClassLesson::setStudents()
+{
+    string new_name;
+    float new_grade ;
+    for (int i = 0; i < countofStudents; i++)
+    {
+        cout << i+1 <<". student: "<< endl; 
+        cout << "Enter his/her name: ";
+        cin >> new_name ;
+        cout << "Enter grade: ";
+        cin >> new_grade;
+        Student a(new_name, new_grade);
+        listofStudents.push_back(a);
+        cout << endl;
+        grades.push_back(new_grade) ;
+    }
+}
+void ClassLesson::sortStudents()
+{
+    for(int i=countofStudents-1 ; i > 0 ; i--)
+    {
+        for(int j = 0 ; j < countofStudents-1 ; j++ )
+        {
+            if(listofStudents.at(j).getGrade() > listofStudents.at(j+1).getGrade() )// should be aware of encapsulation
+            {
+                Student temp = listofStudents.at(j) ;
+                listofStudents.at(j) = listofStudents.at(j+1);
+                listofStudents.at(j+1) = temp ;
+            }
+        }
+    }   
+}
+    
+void ClassLesson::printStudentsName()
+{
+    cout << "Here is the student list witch sorted by their grades :\n";
+    sortStudents();
+    printNames(listofStudents);
+}
+	
+void ClassLesson::printStudentGrade()
+{
+    cout << "Here is their Sorted grades:\n";
+    sort(grades);
+    printGrade(grades);       
+}
+	
